13764d33fc1b156af7a16e5f0036063ff569fd4d
yuhi_deve <yuhinnn@gmail.com>
2016-06-20 23:58:55 +0900

    mod DB.php

--- lib/Base/DB.php
+++ lib/Base/DB.php
@@ -4,6 +4,14 @@ class DB
 {
   //illuminateのデータベース接続設定とブートアップ
   static function registerIlluminate(array $settings){
-
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($settings);
+    $capsule->setEventDispatcher(
+      new \Illuminate\Event\Dispatcher(
+        new \Illuminate\Container\Container()
+      )
+    );
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
   }
 }
