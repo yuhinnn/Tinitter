1e2a5d8971da18e045185bcf48d5807cb204fb0c
yuhi_deve <yuhinnn@gmail.com>
2016-06-27 00:32:56 +0900

    mod TimeLine.php

--- lib/Tinitter/Controller/TimeLine.php
+++ lib/Tinitter/Controller/TimeLine.php
@@ -4,6 +4,13 @@ class TimeLine
 {
   public function show(){
     $app = \Slim\Slim::getInstance();
+    //datebase
+    $post = new \Tinitter\Model\Post; //新規にpostオブジェクトを作成
+    $post->nickname = 'ニックネーム'; //nicknameカラムにセット
+    $post->body = '本文';             //bodyカラムにセット
+    $post->save();                    //保存を実行
+    $same_post = \Tinitter\Model\Post::find(1); //ID=1のデータを1件取得
+    //ここまで
     $app->render(
       'index.twig',
       ['display_text'=>"Hello,world!"]
